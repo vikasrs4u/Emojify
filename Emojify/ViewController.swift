5938fdbf44fc8d7e563a945f1829efd3245fe8f2
Vikas Shetty <vikasrs4u@gmail.com>
2018-07-24 19:45:57 -0700

    Added two SwiftyJson and Alamofire POD

--- Emojify/ViewController.swift
+++ Emojify/ViewController.swift
@@ -22,7 +22,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '11.4'
+
+target 'Emojify' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Emojify
+
+pod 'SwiftyJSON'
+pod 'Alamofire'
+
+
+end
